980450bb98833785461ec33f13c3b9f6106b924c
Никита Лобаков <lobakov.nek@gmail.com>
2020-12-04 21:47:01 +0500

    layout fix

--- gulpfile.js
+++ gulpfile.js
@@ -6,26 +6,29 @@ const gulp = require('gulp'),
 
 gulp.task('sass', () => {
   return gulp
-    .src('./style/style.scss')
+    .src('style/style.scss')
     .pipe(sass())
-    .pipe(cssbeautify({
-      indent: '  '
-    }))
-    .pipe(gulp.dest('./style/css/'));
+    .pipe(cssbeautify({ indent: '  ' }))
+    .pipe(gulp.dest('style/css/'));
 });
 
 gulp.task('sass-min', () => {
   return gulp
-    .src('./style/style.scss')
+    .src('style/style.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./style/css/'));
+    .pipe(gulp.dest('style/css/'));
 });
 
-gulp.task(
-  'default',
-  gulp.series('sass', 'sass-min', (cb) => {
-    gulp.watch('./style/*.scss', gulp.series('sass', 'sass-min'));
-    cb();
-  })
+gulp.task('build', 
+  gulp.series('sass', 'sass-min')
+);
+
+gulp.task('watch', () => {
+  gulp.watch(['style/*.scss'], gulp.series('build'));
+  gulp.watch(['style/components/*.scss'], {delay: 500}, gulp.series('build'));
+});
+
+gulp.task('default',
+  gulp.series('build', 'watch')
 );
