8c49b3df1862a4dba40adcd5e9221bc8a28d0b47
Teaseek <lobakov.nek@gmail.com>
2022-02-20 19:07:39 +0500

    gulp fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
-const { task, dest, src, series, watch } = require('gulp'),
+const { task, dest, src, series, watch } = require('gulp')
   sass = require('gulp-sass')(require('sass')),
   rename = require('gulp-rename'),
   cleanCSS = require('gulp-clean-css'),
+  shorthand = require('gulp-shorthand'),
+  csso = require('gulp-csso'),
+  del = require('del'),
+  gcmq = require('gulp-group-css-media-queries'),
   autoprefixer = require('gulp-autoprefixer');
 
-var path = {
+const path = {
   scss: {
     src: {
       main: 'style/style.scss',
@@ -18,26 +22,34 @@ var path = {
   }
 };
 
-task('sass', () => {
+const sassBuild = () => {
   return src(path.scss.src.main)
     .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gcmq())
     .pipe(autoprefixer())
     .pipe(cleanCSS({ format: 'beautify' }))
     .pipe(dest(path.scss.dest));
-});
+};
 
-task('css-min', () => {
+const cssMin = () => {
   return src(path.css.src)
-    .pipe(cleanCSS())
+    .pipe(shorthand())
+    .pipe(csso())
     .pipe(rename({ suffix: '.min' }))
     .pipe(dest(path.css.dest));
-});
+};
 
-const build = series('sass', 'css-min');
+const clear = () => {
+  return del(path.css.dest)
+}
 
-task('watch', () => {
+const build = series(clear, sassBuild, cssMin);
+
+const watchFiles = () => {
   watch(path.scss.src.main, build);
   watch(path.scss.src.components, { delay: 500 }, build);
-});
+};
 
-task('default', series(build, 'watch'));
+exports.clear = clear;
+exports.build = build;
+exports.watch = series(build, watchFiles);
